b9dab06ea7aec95cc5d8b9f3b4881a3d70b14b84
jiikko <n905i.1214@gmail.com>
2021-06-26 14:47:48 +0900

    そとにだす

--- src/setting.tsx
+++ src/setting.tsx
@@ -8,17 +8,26 @@ interface Button {
 
 type Prop = {
   buttons: Array<string>;
+  prefixKey: Array<string>;
 };
 
-export const Setting: React.FC<Prop> = ({ buttons }) => {
+export const Setting: React.FC<Prop> = ({ buttons, prefixKey }) => {
+  const [currentLayer, setCurrentLayer] = useState("up")
+  const [currentPrefixKey, setCurrentPrefixKey] = useState(prefixKey)
 
   return (
     <>
-      <div>プレフィックスキー</div>
+      <div>設定中のプレフィックスキー</div>
+      {currentPrefixKey.join(", ")}
+      <hr />
       <div>layer up</div>
       <div>layer right</div>
       <div>layer down</div>
       <div>layer left</div>
+      表示中のlayer{currentLayer}
+      <br />
+
+      <hr />
       <div>available plugins</div>
       <div>available mode</div>
       <div>key setting</div>
--- src/top.tsx
+++ src/top.tsx
@@ -7,12 +7,16 @@ type Prop = {
 const buttons = [
   "a", "b", "x", "y", "up", "right", "down", "left", "r", "l", "zr", "zl",
 ]
+// TODO serverから取得する
+const prefixKey = [
+  "r", "l", "zr", "zl",
+]
 
 export const Top: React.FC<Prop> = () => {
   return (
     <>
       <h2>設定ファイルの変更</h2>
-      <Setting buttons={buttons} />
+      <Setting buttons={buttons} prefixKey={prefixKey} />
     </>
   );
 };
