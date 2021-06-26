67af5c086782e70528ed3e75d347b36889dfe9ec
jiikko <n905i.1214@gmail.com>
2021-06-26 14:52:13 +0900

    toggle

--- src/setting_button.tsx
+++ src/setting_button.tsx
@@ -1,14 +1,25 @@
 import * as React from "react";
+import { useState } from "react";
 
 type Prop = {
   name: string;
 };
 
 export const SettingButton: React.FC<Prop> = ({ name }) => {
+  const [openMenu, toggleMenu] = useState(false);
+  const handleToggle = () => {
+    toggleMenu(!openMenu);
+  }
+
   return (
     <>
-      <li>
+      <li onClick={handleToggle}>
         {name}です
+        {openMenu &&
+        <div>
+          詳細です
+        </div>
+        }
       </li>
     </>
   );
