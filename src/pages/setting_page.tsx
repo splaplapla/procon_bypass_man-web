20c1bc0a61433bbd231c27e239fc96d672323bf4
jiikko <n905i.1214@gmail.com>
2021-07-04 13:41:49 +0900

    apiのレスポンスを表示する

--- src/pages/setting_page.tsx
+++ src/pages/setting_page.tsx
@@ -1,10 +1,11 @@
 /** @jsx jsx */
 
 import { jsx } from '@emotion/react'
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { css } from '@emotion/react'
 import { Setting } from "../components/setting";
 import { Button } from "../types/button";
+import { HttpClient } from "../lib/http_client";
 
 type Prop = {};
 
@@ -16,11 +17,22 @@ const prefixKey: Array<Button> = [
   "r", "l", "zr", "zl",
 ]
 
+const httpClient = new HttpClient();
 export const SettingPage = ({}:Prop) => {
+  const [debugConsole, setDebugConsole] = useState("")
+
+  useEffect(() => {
+    httpClient.getSetting()
+      .then(function (response) {
+        setDebugConsole(response.data.setting);
+      })
+  }, [])
+
   return (
     <>
       <hr />
       <h2>設定ファイルの変更</h2>
+      {debugConsole}
       <Setting buttons={buttons} prefixKey={prefixKey} />
     </>
   )
