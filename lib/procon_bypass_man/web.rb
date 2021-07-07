e9c774fb3a716b7afd9baee38d7e01f48125fabe
jiikko <n905i.1214@gmail.com>
2021-07-07 14:22:25 +0900

    起動時にDBを作成・参照する

--- lib/procon_bypass_man/web.rb
+++ lib/procon_bypass_man/web.rb
@@ -8,5 +8,11 @@ module ProconBypassMan
     def self.root
       File.expand_path('../..', File.dirname(__FILE__))
     end
+
+    def self.connect_db
+      @@db ||= SQLite3::Database.new "pbm_web.db"
+    end
+
+    connect_db
   end
 end
