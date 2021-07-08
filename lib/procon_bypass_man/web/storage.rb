502dbb7bc1e8b0805cb937c3dc089d3e28d67633
jiikko <n905i.1214@gmail.com>
2021-07-09 08:11:47 +0900

    move

--- lib/procon_bypass_man/web/storage.rb
+++ lib/procon_bypass_man/web/storage.rb
@@ -1,29 +1,6 @@
 module ProconBypassMan
   module Web
     class Storage
-      def self.db
-        # TODO connection cache
-        SQLite3::Database.new(ProconBypassMan::Web.config[:db_path])
-      end
-
-      def self.migrate_if_pending_migration(migrations_path: File.join(ProconBypassMan::Web.root, 'lib', 'procon_bypass_man/web', 'migration', "/*.sql"))
-        db.execute <<~SQL
-          create table if not exists "schema_migrations" ("version" varchar not null primary key)
-        SQL
-
-        Dir.glob(migrations_path).each do |path|
-          if /^(\d+)_[\w.]+$/ =~ Pathname.new(path).basename.to_s
-            version = $1
-            rows = db.execute("select * from schema_migrations where version = ?", version)
-            if rows.size == 0
-              sql = File.read(path)
-              db.execute(sql)
-              db.execute("insert into schema_migrations (version) values (?)", [version])
-            end
-          end
-        end
-      end
-
       def pbm_dir_path
         read(:pbm_dir_path)
       end
--- spec/lib/db_spec.rb
+++ spec/lib/db_spec.rb
@@ -1,10 +1,11 @@
 require "spec_helper"
 
-describe ProconBypassMan::Web::Storage do
+describe ProconBypassMan::Web::Db do
   describe '.migrate_if_pending_migration' do
     before do
       ProconBypassMan::Web::Db.recreate!
     end
+
     it do
       expect {
         ProconBypassMan::Web::Db.db.execute("select * from users")
@@ -12,17 +13,6 @@ describe ProconBypassMan::Web::Storage do
       ProconBypassMan::Web::Db.migrate_if_pending_migration(migrations_path: "spec/lib/migration/*.sql")
       expect(ProconBypassMan::Web::Db.db.execute("select * from schema_migrations").size).to eq(1)
       expect(ProconBypassMan::Web::Db.db.execute("select * from users").size).to eq(0)
-    end
-  end
-
-  describe '#pbm_dir_path' do
-    before do
-      ProconBypassMan::Web::Db.recreate!
-      ProconBypassMan::Web::Db.migrate_if_pending_migration
-    end
-    it do
-      ProconBypassMan::Web::Db.db.execute("select * from settings")
-      # ProconBypassMan::Web::Storage.instance.pbm_dir_path
     end
   end
 end
