eed56e7748b60079b9136cdb3f49abba74ad10b2
jiikko <n905i.1214@gmail.com>
2021-08-04 20:50:53 +0900

    マージンを開ける

--- src/pages/buttons_setting_page.tsx
+++ src/pages/buttons_setting_page.tsx
@@ -93,26 +93,28 @@ export const ButtonsSettingPage = () => {
   }, [loaded]);
 
   const layersTabStyle = () => {
-  return css`
-    ul {
-      list-style: none;
-      display:flex;
-      margin: 0;
-      padding: 0;
-      border-left: 1px solid #aaa;
-      li {
-        padding: 20px;
-        border-top: 1px solid #aaa;
-        border-right: 1px solid #aaa;
-        &.active {
-          border-bottom: 1px solid #white;
-        }
-        &.inactive {
-          border-bottom: 1px solid #aaa;
+    return css`
+      ul {
+        list-style: none;
+        display:flex;
+        margin: 0;
+        padding: 0;
+        border-left: 1px solid #aaa;
+        margin-bottom: 30px;
+
+        li {
+          padding: 20px;
+          border-top: 1px solid #aaa;
+          border-right: 1px solid #aaa;
+          &.active {
+            border-bottom: 1px solid #white;
+          }
+          &.inactive {
+            border-bottom: 1px solid #aaa;
+          }
         }
       }
-    }
-  `;
+    `;
   }
   const liClassName = (layer: LayerKey) => {
     if(layer === selectedLayer) {
