3376eba4c2ad2ef02fe40fe1683724b291d2b1b3
jiikko <n905i.1214@gmail.com>
2021-08-06 23:12:21 +0900

    nestd cssをやめる

--- src/components/buttons_setting.tsx
+++ src/components/buttons_setting.tsx
@@ -4,56 +4,51 @@ import { jsx, css } from '@emotion/react'
 import React, { useState } from "react";
 import { ButtonSetting } from "./button_setting";
 import { Button, buttons } from "../types/button";
+import { LayerKey } from "../types/layer_key";
 import { ButtonsSettingContext } from "./../contexts/buttons_setting";
 
 type Props = {
-  layerKey: string;
+  layerKey: LayerKey;
   layerRef: any;
 };
 
 export const ButtonsSetting = ({ layerKey, layerRef }:Props) => {
   const [visibility, setVisibility] = useState("hidden");
-  const visibilityClassName = () => {
+  const visibilityStyle = () => {
     if(visibility === "hidden") {
-      return "hidden"
-    } else {
-      return "shown"
+      return css`display: none;`;
     }
-  };
-  const style = () => {
+  }
+  const ulStyle = () => {
     return css`
-      &.hidden {
-        display: none;
-      }
-      ul.buttons_setting {
-        border: 1px solid #666; display: flex;
-        flex-wrap: wrap;
-        justify-content: center;
-        list-style-type: none;
-        margin: 0 0 0 1em;
-        padding: 0;
-        width: 900px;
-        li.buttons_setting {
-          border: 1px solid #aaa;
-          display: table;
-          line-height: 110%;
-          margin: 0.2em;
-          padding: 0.5em;
-          text-align: center;
-          width: 200px;
-          height: 100px;
-        }
-      }
+      border: 1px solid #666; display: flex;
+      flex-wrap: wrap;
+      justify-content: center;
+      list-style-type: none;
+      margin: 0 0 0 1em;
+      padding: 0;
+      width: 900px;
     `;
   };
-
+  const liStyle = () => {
+    return css`
+      border: 1px solid #aaa;
+      display: table;
+      line-height: 110%;
+      margin: 0.2em;
+      padding: 0.5em;
+      text-align: center;
+      width: 200px;
+      height: 100px;
+    `;
+  }
   layerRef.setVisibility = setVisibility;
 
   return (
-    <div className={visibilityClassName()} css={style()}>
-      <ul className={"buttons_setting"} >
+    <div css={visibilityStyle()}>
+      <ul css={ulStyle()}>
         {buttons.map((b, i) => (
-          <li key={i} className={"buttons_setting"} >
+          <li key={i} css={liStyle()}>
             <ButtonSetting layerKey={layerKey} name={b} />
           </li>
         ))}
