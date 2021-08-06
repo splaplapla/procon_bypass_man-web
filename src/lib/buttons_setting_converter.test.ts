eef96b1357898798caddc7e61089d2f582582087
jiikko <n905i.1214@gmail.com>
2021-08-07 07:43:13 +0900

    yamlをパースする

--- src/lib/buttons_setting_converter.test.ts
+++ src/lib/buttons_setting_converter.test.ts
@@ -1,10 +1,29 @@
 import { ButtonsSettingConverter } from "./buttons_setting_converter";
 import { Button, buttons } from "../types/button";
 import { ButtonsInLayer } from "../types/buttons_setting_type";
+import yaml from "js-yaml";
 
 describe('値があるとき', () => {
   it('validなyamlであること', () => {
-    // TODO
+    const prefixKeys = ["y", "l"] as Array<Button>;
+    const defaultLayer = buttons.reduce((acc, item) => { acc[item] = { open: false }; return acc; }, {} as ButtonsInLayer);
+    const upLayer =  Object.assign({}, defaultLayer);
+    const downLayer = Object.assign({}, defaultLayer);
+    const leftLayer = Object.assign({}, defaultLayer);
+    const rightLayer = Object.assign({}, defaultLayer);
+    upLayer.a = { flip: { if_pressed: [], enable: true, force_neutral: ["y"] }, open: true }
+    upLayer.b = { flip: { if_pressed: ["a"], enable: true, force_neutral: ["y"] }, open: true }
+    upLayer.x = { flip: { if_pressed: ["a"], enable: false, force_neutral: ["y"] }, open: true }
+    upLayer.y = { flip: { if_pressed: [], enable: false, force_neutral: [] }, open: true }
+    downLayer.a = { flip: { if_pressed: ["a", "b"], enable: true, force_neutral: ["y", "x"] }, open: true }
+    const layers = {
+      up: upLayer,
+      down: downLayer,
+      left: leftLayer,
+      right: rightLayer,
+    };
+    const actual = ButtonsSettingConverter({ prefixKeys: prefixKeys, layers: layers })
+    yaml.load(actual)
   })
 
   it('定義が出力されること', () => {
@@ -46,7 +65,20 @@ setting: |-
 
 describe('全部からのとき', () => {
   it('validなyamlであること', () => {
-    // TODO
+    const prefixKeys = ["y", "l"] as Array<Button>;
+    const defaultLayer = buttons.reduce((acc, item) => { acc[item] = { open: false }; return acc; }, {} as ButtonsInLayer);
+    const upLayer = defaultLayer;
+    const downLayer = defaultLayer;
+    const leftLayer = defaultLayer;
+    const rightLayer = defaultLayer;
+    const layers = {
+      up: upLayer,
+      down: downLayer,
+      left: leftLayer,
+      right: rightLayer,
+    };
+    const actual = ButtonsSettingConverter({ prefixKeys: prefixKeys, layers: layers })
+    yaml.load(actual)
   })
 
   it('全部空になること', () => {
