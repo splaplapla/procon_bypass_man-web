376e026f4dcf37635f83003b4dbafefe842744cf
jiikko <n905i.1214@gmail.com>
2021-08-07 10:09:53 +0900

    add test

--- src/lib/button_state_diff.test.ts
+++ src/lib/button_state_diff.test.ts
@@ -2,7 +2,7 @@ import { Button, buttons } from "../types/button";
 import { ButtonInLayer, ButtonsInLayer, Layers, ButtonsSettingType } from "../types/buttons_setting_type";
 import { ButtonStateDiff } from "./button_state_diff";
 
-describe('', () => {
+describe('全部ブランクの時', () => {
   it('[]を返す', () => {
     const defaultLayer = buttons.reduce((acc, item) => { acc[item] = { open: false }; return acc; }, {} as ButtonsInLayer);
     const upLayer =  Object.assign({}, defaultLayer);
@@ -13,6 +13,58 @@ describe('', () => {
     const after = { prefix_keys_for_changing_layer: [], layers: { up: upLayer, down: downLayer, left: leftLayer, right: rightLayer  } }
 
     const actual = ButtonStateDiff({ before: before, after: after })
-    expect(actual).toStrictEqual([])
+    expect(actual).toStrictEqual({"changes": []})
+  })
+})
+
+describe('keyで差分がある時', () => {
+  it('値を返す', () => {
+    const defaultLayer = buttons.reduce((acc, item) => { acc[item] = { open: false }; return acc; }, {} as ButtonsInLayer);
+    const upLayer =  Object.assign({}, defaultLayer);
+    const downLayer = Object.assign({}, defaultLayer);
+    const leftLayer = Object.assign({}, defaultLayer);
+    const rightLayer = Object.assign({}, defaultLayer);
+    const defaultSetting = { prefix_keys_for_changing_layer: [] as Array<Button>, layers: { up: upLayer, down: downLayer, left: leftLayer, right: rightLayer  } }
+    const before = Object.assign({}, defaultSetting);
+    const after = Object.assign({}, defaultSetting);
+    before.prefix_keys_for_changing_layer = ["a"];
+    after.prefix_keys_for_changing_layer = ["y", "x"];
+
+    const actual = ButtonStateDiff({ before: before, after: after })
+    expect(actual).toStrictEqual({ changes: ["keyprefixは a => y,x になります"] })
+  })
+})
+
+describe('layersで差分がある時', () => {
+  const makeLayer = () => {
+    const defaultLayer = buttons.reduce((acc, item) => { acc[item] = { open: false }; return acc; }, {} as ButtonsInLayer);
+    const upLayer =  Object.assign({}, defaultLayer);
+    const downLayer = Object.assign({}, defaultLayer);
+    const leftLayer = Object.assign({}, defaultLayer);
+    const rightLayer = Object.assign({}, defaultLayer);
+    return {
+      prefix_keys_for_changing_layer: [] as Array<Button>,
+      layers: {
+        up: Object.assign({}, upLayer),
+        down: Object.assign({}, downLayer),
+        left: Object.assign({}, leftLayer),
+        right: Object.assign({}, rightLayer),
+      }
+    };
+  };
+  it('値を返す', () => {
+    const before = Object.assign({}, makeLayer());
+    const after = Object.assign({}, makeLayer());
+    before.prefix_keys_for_changing_layer = ["a"];
+    after.prefix_keys_for_changing_layer = ["a"];
+    before.layers.up.a = { flip: { if_pressed: ["y"], enable: true }, open: true }
+    before.layers.down.a = { flip: { if_pressed: ["y"], enable: true }, open: true }
+    after.layers.up.a = { flip: { if_pressed: ["a"], enable: false }, open: true }
+
+    const actual = ButtonStateDiff({ before: before, after: after })
+    expect(actual).toStrictEqual({"changes": [
+      "layer up の a を変更しました",
+      "layer down の a を変更しました",
+    ]})
   })
 })
--- src/lib/button_state_diff.ts
+++ src/lib/button_state_diff.ts
@@ -1,11 +1,34 @@
-import { Button } from "../types/button";
+import { Button, buttons } from "../types/button";
 import { ButtonInLayer, ButtonsInLayer, Layers, ButtonsSettingType } from "../types/buttons_setting_type";
+import { LayerKey, layerKeys } from "../types/layer_key";
+import { diff } from 'deep-object-diff';
 
 type Props = {
   before: ButtonsSettingType;
   after: ButtonsSettingType;
 };
 
+export type Diff = {
+  changes: Array<string>;
+}
+
 export const ButtonStateDiff = ({ before, after }: Props) => {
-  return [];
+  const result = { changes: [] } as Diff;
+  if(before.prefix_keys_for_changing_layer.toString() === after.prefix_keys_for_changing_layer.toString()) {
+    // no-op
+  } else {
+    result.changes.push(`keyprefixは ${before.prefix_keys_for_changing_layer} => ${after.prefix_keys_for_changing_layer} になります`)
+  }
+
+  layerKeys.forEach((layerKey) => {
+    buttons.forEach((b) => {
+      // どの項目が何に変化したかを出力したかったがめんどくさすぎたのでやめた
+      const diffResult = diff(before.layers[layerKey as LayerKey][b], after.layers[layerKey as LayerKey][b]);
+      if(Object.keys(diffResult).length > 0) {
+        result.changes.push(`layer ${layerKey} の ${b} を変更しました`)
+      }
+    })
+  })
+
+  return result;
 }
