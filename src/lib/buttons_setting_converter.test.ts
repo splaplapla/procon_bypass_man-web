af49dcee9b742d71a1018dbb4fe1d20c06a71104
jiikko <n905i.1214@gmail.com>
2021-08-11 20:34:37 +0900

    ちょっとまとめる

--- src/lib/buttons_setting_converter.test.ts
+++ src/lib/buttons_setting_converter.test.ts
@@ -2,14 +2,15 @@ import { ButtonsSettingConverter } from "./buttons_setting_converter";
 import { Button, buttons } from "../types/button";
 import { ButtonsInLayer } from "../types/buttons_setting_type";
 import yaml from "js-yaml";
+import _ from 'lodash';
 
 describe('値があるとき', () => {
   const prefixKeys = ["y", "l"] as Array<Button>;
   const defaultLayer = buttons.reduce((acc, item) => { acc[item] = { open: false }; return acc; }, {} as ButtonsInLayer);
-  const upLayer =  Object.assign({}, defaultLayer);
-  const downLayer = Object.assign({}, defaultLayer);
-  const leftLayer = Object.assign({}, defaultLayer);
-  const rightLayer = Object.assign({}, defaultLayer);
+  const upLayer =  _.cloneDeep(defaultLayer);
+  const downLayer = _.cloneDeep(defaultLayer);
+  const leftLayer = _.cloneDeep(defaultLayer);
+  const rightLayer = _.cloneDeep(defaultLayer);
   upLayer.a = { flip: { if_pressed: [], enable: true, force_neutral: ["y"] }, open: true }
   upLayer.b = { flip: { if_pressed: ["a"], enable: true, force_neutral: ["y"] }, open: true }
   upLayer.x = { flip: { if_pressed: ["a"], enable: false, force_neutral: ["y"] }, open: true }
@@ -49,18 +50,17 @@ setting: |-
   })
 })
 
+describe('macroがあるとき', () => {
+})
+
 describe('全部からのとき', () => {
   const prefixKeys = ["y", "l"] as Array<Button>;
   const defaultLayer = buttons.reduce((acc, item) => { acc[item] = { open: false }; return acc; }, {} as ButtonsInLayer);
-  const upLayer = defaultLayer;
-  const downLayer = defaultLayer;
-  const leftLayer = defaultLayer;
-  const rightLayer = defaultLayer;
   const layers = {
-    up: upLayer,
-    down: downLayer,
-    left: leftLayer,
-    right: rightLayer,
+    up: defaultLayer,
+    down: defaultLayer,
+    left: defaultLayer,
+    right: defaultLayer,
   };
 
   it('validなyamlであること', () => {
