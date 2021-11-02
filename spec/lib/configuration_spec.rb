212ab0ab1f7ba12aa47f95dd2dda3b37dc557ee1
jiikko <n905i.1214@gmail.com>
2021-11-03 02:22:12 +0900

    Fix pwd

--- spec/lib/configuration_spec.rb
+++ spec/lib/configuration_spec.rb
@@ -7,7 +7,7 @@ describe ProconBypassMan::Web::Configuration do
         config.root = File.expand_path('../..', __dir__).freeze
         config.logger = Logger.new("#{ProconBypassMan::Web.root}/web.log", 1, 1024 * 1024 * 10)
       end
-      expect(ProconBypassMan::Web.config.root).to eq("/Users/koji/src/procon_bypass_man-web")
+      expect(ProconBypassMan::Web.config.root).to eq(`pwd`.chomp)
       expect(ProconBypassMan::Web.config.logger).to eq(ProconBypassMan::Web.logger)
       expect(ProconBypassMan::Web.logger).to be_a(Logger)
     end
